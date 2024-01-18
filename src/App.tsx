1a5d21ffcef6609c906d8554893c05488a7edea4
silvialcastilla <silope0302@gmail.com>
2024-01-18 21:58:33 +0100

    tvup with dev container

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sumNum = (num1: number, num2: number) => {
+
+    const result = num1+num2;
+
+    return result;
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{sumNum(1,2)}</div>
     </div>
   );
 }
